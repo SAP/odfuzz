2cd5838b10e4791be921b52edd9f370f40de4acc
lubosmj <lubos.mjachky@gmail.com>
2018-04-10 23:31:00 +0200

    Implement remaining string mutations, rename methods, add base charset

--- odfuzz/generators.py
+++ odfuzz/generators.py
@@ -2,6 +2,10 @@
 
 import random
 
+BASE_CHARSET = 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789~!$@^*()_+-–—=' \
+               '[]|:<>?,./‰¨œƒ…†‡Œ‘’´`“”•™¡¢£¤¥¦§©ª«¬®¯°±²³µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍ' \
+               'ÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ{} '
+
 
 class StringMutator(object):
     @staticmethod
@@ -24,21 +28,32 @@ class StringMutator(object):
         index2 = round(random.random() * index_length)
         while index1 == index2:
             index2 = round(random.random() * index_length)
+
         list_char = list(string)
         list_char[index1], list_char[index2] = string[index2], string[index1]
         return ''.join(list_char)
 
     @staticmethod
-    def invert_chars():
-        pass
+    def invert_chars(string):
+        index_len = len(string) - 1
+        start_index = round(random.random() * (index_len - 2))
+        end_index = round(random.random() * (index_len - start_index - 1)) + start_index + 2
+        slice_char = string[start_index:end_index]
+
+        list_char = list(string)
+        list_char[start_index:end_index] = slice_char[::-1]
+        return ''.join(list_char)
 
     @staticmethod
-    def add_char():
-        pass
+    def add_char(string):
+        index = round(random.random() * (len(string)))
+        new_char = random.choice(BASE_CHARSET)
+        return ''.join([string[:index], new_char, string[index:]])
 
     @staticmethod
-    def delete_char():
-        pass
+    def delete_char(string):
+        index = round(random.random() * (len(string) - 1))
+        return ''.join([string[:index], string[index + 1:]])
 
 
 class NumberMutator(object):
@@ -48,16 +63,16 @@ class NumberMutator(object):
 
     @staticmethod
     def increment_value(number):
-        pass
+        return number + 1
 
     @staticmethod
     def decrement_value(number):
+        return number - 1
+
+    @staticmethod
+    def add_digit(number):
         pass
 
     @staticmethod
-    def add_value():
-        pass
-
-    @staticmethod
-    def remove_value():
+    def delete_digit(number):
         pass
