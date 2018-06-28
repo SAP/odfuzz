97170835632dd65ea2dcba25af7db978ca1cb29c
lubosmj <lubos.mjachky@gmail.com>
2018-06-28 10:51:29 +0200

    Add tests for pyodata layer, fix bugs

---
+++ tests/conftest.py
@@ -0,0 +1,77 @@
+import pytest
+
+from pyodata.v2.model import Edmx
+from odfuzz.arguments import ArgParser
+
+
+@pytest.fixture
+def metadata():
+    return """
+        <edmx:Edmx xmlns:edmx="http://schemas.microsoft.com/ado/2007/06/edmx" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" xmlns:sap="http://www.sap.com/Protocols/SAPData" Version="1.0">
+         <edmx:Reference xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Uri="https://example.sap.corp/sap/opu/odata/IWFND/CATALOGSERVICE;v=2/Vocabularies(TechnicalName='%2FIWBEP%2FVOC_COMMON',Version='0001',SAP__Origin='LOCAL')/$value">
+          <edmx:Include Namespace="com.sap.vocabularies.Common.v1" Alias="Common"/>
+         </edmx:Reference>
+         <edmx:DataServices m:DataServiceVersion="2.0">
+          <Schema xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" xmlns="http://schemas.microsoft.com/ado/2008/09/edm" Namespace="EXAMPLE_SRV" xml:lang="en" sap:schema-version="1">
+           <EntityType Name="MasterEntity" sap:content-version="1">
+            <Key>
+             <PropertyRef Name="Key"/>
+            </Key>
+            <Property Name="Key" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Key" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:value-list="standard"/>
+            <Property Name="DataType" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Key" sap:creatable="false" sap:updatable="false" sap:sortable="false"/>
+            <Property Name="Data" Type="Edm.String" MaxLength="Max" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false" sap:text="DataName"/>
+            <Property Name="DataName" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false"/>
+           </EntityType>
+           <EntityType Name="DataEntity" sap:content-version="1" sap:value-list="true" sap:label="Data entities">
+            <Key>
+             <PropertyRef Name="Name"/>
+            </Key>
+            <Property Name="Name" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false"/>
+            <Property Name="Type" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false"/>
+            <Property Name="Value" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false"/>
+            <Property Name="Description" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false"/>
+            <Property Name="Invisible" Type="Edm.String" Nullable="false" sap:unicode="false" sap:label="Data" sap:creatable="false" sap:updatable="false" sap:sortable="false" sap:filterable="false" sap:visible="false"/>
+           </EntityType>
+           <Association Name="toDataEntity" sap:content-version="1">
+            <End Type="EXAMPLE_SRV.MasterEntity" Multiplicity="1" Role="FromRole_toDataEntity" />
+            <End Type="EXAMPLE_SRV.DataEntity" Multiplicity="*" Role="ToRole_toDataEntity" />
+            <ReferentialConstraint>
+             <Principal Role="FromRole_toDataEntity">
+              <PropertyRef Name="Key" />
+             </Principal>
+             <Dependent Role="ToRole_toDataEntity">
+              <PropertyRef Name="Name" />
+             </Dependent>
+            </ReferentialConstraint>
+           </Association>
+           <Association Name="toMasterEntity" sap:content-version="1">
+            <End Type="EXAMPLE_SRV.MasterEntity" Multiplicity="1" Role="FromRole_toMasterEntity" />
+            <End Type="EXAMPLE_SRV.MasterEntity" Multiplicity="0..1" Role="ToRole_toMasterEntity" />
+           </Association>
+           <EntityContainer Name="EXAMPLE_SRV" m:IsDefaultEntityContainer="true" sap:supported-formats="atom json xlsx">
+            <EntitySet Name="MasterSet" EntityType="EXAMPLE_SRV.MasterEntity" sap:creatable="false" sap:updatable="false" sap:deletable="false" sap:searchable="true" sap:content-version="1"/>
+            <EntitySet Name="DataSet" EntityType="EXAMPLE_SRV.DataEntity" sap:creatable="false" sap:updatable="false" sap:deletable="false" sap:searchable="true" sap:content-version="1" sap:addressable="false"/>
+            <AssociationSet Name="toDataEntitySet" Association="EXAMPLE_SRV.toDataEntity" sap:creatable="false" sap:updatable="false" sap:deletable="false" sap:content-version="1">
+             <End EntitySet="MasterSet" Role="FromRole_toDataEntity" />
+             <End EntitySet="DataSet" Role="ToRole_toDataEntity" />
+            </AssociationSet>
+            <AssociationSet Name="toMasterEntitySet" Association="EXAMPLE_SRV.toMasterEntity" sap:creatable="false" sap:updatable="false" sap:deletable="false" sap:content-version="1">
+             <End EntitySet="MasterSet" Role="FromRole_toMasterEntity" />
+             <End EntitySet="MasterSet" Role="ToRole_toMasterEntity" />
+            </AssociationSet>
+           </EntityContainer>
+          </Schema>
+         </edmx:DataServices>
+        </edmx:Edmx>"""
+
+
+@pytest.fixture
+def schema(metadata):
+    parsed_schema = Edmx.parse(metadata)
+    return parsed_schema
+
+
+@pytest.fixture
+def argparser():
+    argument_parser = ArgParser()
+    return argument_parser
---
+++ tests/test_pyodata.py
@@ -0,0 +1,82 @@
+def test_entity_type_parsing(schema):
+    entity_types = [entity_type.name for entity_type in schema.entity_types]
+    assert set(entity_types) == {'MasterEntity', 'DataEntity'}
+
+
+def test_master_entity_properties_parsing(schema):
+    master_entity = schema.entity_type('MasterEntity')
+    master_entity_properties = [proprty.name for proprty in master_entity.proprties()]
+    assert set(master_entity_properties) == {'DataType', 'Data', 'DataName', 'Key'}
+
+
+def test_data_entity_properties_parsing(schema):
+    data_entity = schema.entity_type('DataEntity')
+    data_entity_properties = [proprty.name for proprty in data_entity.proprties()]
+    assert set(data_entity_properties) == {'Name', 'Type', 'Value', 'Description', 'Invisible'}
+
+
+def test_entity_set_parsing(schema):
+    entity_sets = [entity_set.name for entity_set in schema.entity_sets]
+    assert set(entity_sets) == {'DataSet', 'MasterSet'}
+
+
+def test_master_set_attributes(schema):
+    master_set = schema.entity_set('MasterSet')
+    assert master_set.searchable
+    assert master_set.addressable
+
+
+def test_data_set_attributes(schema):
+    data_set = schema.entity_set('DataSet')
+    assert data_set.searchable
+    assert not data_set.addressable
+
+
+def test_master_entity_key_properties_parsing(schema):
+    master_key_properties = schema.entity_type('MasterEntity').key_proprties
+    assert set(key_property.name for key_property in master_key_properties) == {'Key'}
+
+
+def test_data_entity_key_properties_parsing(schema):
+    data_key_proprties = schema.entity_type('DataEntity').key_proprties
+    assert set(key_property.name for key_property in data_key_proprties) == {'Name'}
+
+
+def test_master_to_data_association_parsing(schema):
+    master_to_data_association = schema.association('toDataEntity')
+
+    association_end_roles = [end.role for end in master_to_data_association.end_roles]
+    assert set(association_end_roles) == {'FromRole_toDataEntity', 'ToRole_toDataEntity'}
+
+    association_end_entity_types = [end.entity_type.name for end in master_to_data_association.end_roles]
+    assert set(association_end_entity_types) == {'MasterEntity', 'DataEntity'}
+
+
+def test_master_to_master_association_parsing(schema):
+    master_to_master_association = schema.association('toMasterEntity')
+
+    association_end_roles = [end.role for end in master_to_master_association.end_roles]
+    assert set(association_end_roles) == {'FromRole_toMasterEntity', 'ToRole_toMasterEntity'}
+
+    association_end_entity_types = [end.entity_type.name for end in master_to_master_association.end_roles]
+    assert set(association_end_entity_types) == {'MasterEntity', 'MasterEntity'}
+
+
+def test_master_to_data_association_set_parsing(schema):
+    master_to_data_association_set = schema.association_set('toDataEntitySet')
+
+    association_set_end_roles = [end.role for end in master_to_data_association_set.ends]
+    assert set(association_set_end_roles) == {'FromRole_toDataEntity', 'ToRole_toDataEntity'}
+
+    association_set_end_entity_sets = [end.entity_set for end in master_to_data_association_set.ends]
+    assert set(association_set_end_entity_sets) == {'MasterSet', 'DataSet'}
+
+
+def test_master_to_master_association_set_parsing(schema):
+    master_to_master_association_set = schema.association_set('toMasterEntitySet')
+
+    association_set_end_roles = [end.role for end in master_to_master_association_set.ends]
+    assert set(association_set_end_roles) == {'FromRole_toMasterEntity', 'ToRole_toMasterEntity'}
+
+    association_set_end_entity_sets = [end.entity_set for end in master_to_master_association_set.ends]
+    assert set(association_set_end_entity_sets) == {'MasterSet', 'MasterSet'}
