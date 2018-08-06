6660e75772046a3da5329a1213afee7cba209323
lubosmj <lubos.mjachky@gmail.com>
2018-08-06 15:05:02 +0200

    Print help message without raising an exception

--- odfuzz/arguments.py
+++ odfuzz/arguments.py
@@ -1,24 +1,24 @@
 """This module contains a wrapper for parsing command line arguments."""
 
+import sys
 from argparse import ArgumentParser
 
 from odfuzz.exceptions import ArgParserError
 
+FUZZER_DESC = 'Fuzzer for testing applications communicating via the OData protocol'
+
 
 class ArgParser(object):
-    """An argument parser wrapper."""
-
     def __init__(self):
-        self._parser = ArgumentParser(
-            prog='ODfuzz',
-            description='Fuzzer for testing applications communicating via the OData protocol')
+        self._parser = ArgumentParser(prog='ODfuzz', add_help=False, description=FUZZER_DESC)
         self._add_arguments()
 
     def parse(self, arguments):
+        self._handle_help_option(arguments)
         try:
             parsed_arguments = self._parser.parse_args(arguments)
-        except SystemExit as system_exit:
-            raise ArgParserError('ERROR {}: Cannot parse command line arguments'.format(system_exit))
+        except SystemExit:
+            raise ArgParserError('Cannot parse command line arguments')
         return parsed_arguments
 
     def _add_arguments(self):
@@ -26,6 +26,10 @@ class ArgParser(object):
         self._parser.add_argument('-l', '--logs', type=str, help='A logs directory')
         self._parser.add_argument('-s', '--stats', type=str, help='A statistics directory')
         self._parser.add_argument('-r', '--restr', type=str, help='A user defined restrictions')
-        self._parser.add_argument(
-            '-a', '--async', action='store_true', default=False,
-            help='Allow ODfuzz to send HTTP requests asynchronously')
+        self._parser.add_argument('-a', '--async', action='store_true', default=False,
+                                  help='Allow ODfuzz to send HTTP requests asynchronously')
+
+    def _handle_help_option(self, arguments):
+        if '-h' in arguments or '--help' in arguments:
+            self._parser.print_help()
+            sys.exit(0)
