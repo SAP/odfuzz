c0f232c8b080efa9e4ddc4508fd4e17696baa2cd
Petr Hanák <47626173+phanak-sap@users.noreply.github.com>
2019-10-21 14:52:22 +0200

    Fix #24 (#30)
    introduce env var ODFUZZ_SAP_CLIENT

--- odfuzz/config.py
+++ odfuzz/config.py
@@ -1,15 +1,18 @@
 """This module contains classes for fetching and parsing basic configurations."""
 
 import yaml
+import os
 
 from odfuzz.constants import CERTIFICATE_PATH, ASYNC_REQUESTS_NUM, FUZZER_CONFIG_PATH, \
-    SAP_CLIENT, DATA_FORMAT, URLS_PER_PROPERTY
+    SAP_CLIENT, DATA_FORMAT, URLS_PER_PROPERTY, ENV_SAP_CLIENT
 from odfuzz.exceptions import ConfigParserError
 
 
 class FuzzerConfig:
     def __init__(self, config):
-        self._sap_client = config.get('sap_client', SAP_CLIENT)
+        self._sap_client = os.getenv(ENV_SAP_CLIENT, config.get('sap_client', SAP_CLIENT))
+        #overwrite if ENV variable exists - https://github.com/SAP/odfuzz/issues/24
+
         self._data_format = config.get('data_format', DATA_FORMAT)
         self._urls_per_property = config.get('urls_per_property', URLS_PER_PROPERTY)
 
@@ -63,15 +66,15 @@ class Config:
 
     @staticmethod
     def init_from(config_file):
-        config_dict = Config.raw_from(config_file)
+        config_dict = Config._raw_from(config_file)
         if not config_dict:
-            config_dict = Config.raw_from(FUZZER_CONFIG_PATH)
+            config_dict = Config._raw_from(FUZZER_CONFIG_PATH)
 
         Config.fuzzer = FuzzerConfig(config_dict.get('fuzzer') or {})
         Config.dispatcher = DispatcherConfig(config_dict.get('dispatcher') or {})
 
     @staticmethod
-    def raw_from(config_file):
+    def _raw_from(config_file):
         try:
             with open(config_file) as stream:
                 config_dict = yaml.safe_load(stream)
