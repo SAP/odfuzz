15be99699e550f5c747371078190ed94908d3d02
I335851 <petr.hanak@sap.com>
2019-11-12 14:01:08 +0100

    update setup.py to the rest of the codebase

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+version = Path('VERSION').read_text().strip()
 
 setup(
     name='odfuzz',
-    version='0.10.2',
+    version=version,
     license='Apache License Version 2.0',
-    url='https://github.wdf.sap.corp/I342520/ODfuzz',
-    author='Lubos Mjachky',
-    author_email='lubos.mjachky@sap.com',
+    url='https://github.com/SAP/odfuzz',
+    author='Lubos Mjachky, Jakub Filak, Petr Hanak',
+    author_email='jakub.filak@sap.com, petr.hanak@sap.com',
     description='Fuzzer for testing applications communicating via the OData protocol',
     packages=find_packages(exclude=['tests', 'restrictions']),
     include_package_data=True,
@@ -17,14 +20,21 @@ setup(
     },
     zip_safe=False,
     install_requires=[
-        'pytest>=3.5.0',
         'gevent>=1.2.2',
         'requests>=2.18.4',
         'pymongo>=3.6.1',
         'lxml>=3.7.3',
         'pyyaml>=3.13',
         'python-dateutil>=2.7.5',
-        'mongomock>=3.14.0'
+        'pyodata>=1.2.3',
+    ],
+    tests_require=[
+        'mongomock>=3.14.0',
+        'pytest>=3.5.0',
+        'pytest-cov>=2.7.1',
+        'codecov>=2.0.15',
+        'pylint>=1.2.1',
+        'bandit>=1.6.2'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
