f7f8b7cb68798a711a5481ffd934e4ecaaf5f1f6
AlapanGhosh <36377313+AlapanGhosh@users.noreply.github.com>
2021-06-21 12:11:30 +0530

    PUT POST MERGE tests

--- tests/integration/url_generator_only/test_url_generator_only.py
+++ tests/integration/url_generator_only/test_url_generator_only.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import random
 
 from odfuzz.restrictions import RestrictionsGroup
 from odfuzz.entities import DirectBuilder
@@ -90,3 +91,79 @@ def test_direct_builder_http_delete():
     queries_list=set(queries_list)
     choice = queries_list.pop()
     assert ("filter" in choice or "expand" in choice or "startswith" in choice or "replace" in choice or "substring" in choice or "inlinecount" in choice) == False
+
+def test_direct_builder_http_put_url():
+    random.seed(20)
+    path_to_metadata = Path(__file__).parent.joinpath("metadata-northwind-v2.xml")
+    metadata_file_contents = path_to_metadata.read_bytes()
+    restrictions = RestrictionsGroup(None)
+    put_builder = DirectBuilder(metadata_file_contents, restrictions,"PUT")
+    put_entities = put_builder.build()
+    queryable_factory = SingleQueryable
+    queries_list = []
+    queries_list.clear()
+    for queryable in put_entities.all():
+        entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
+        for _ in range(entityset_urls_count):
+            q = queryable_factory(queryable, logger, 1)
+            queries,body = q.generate()
+            queries_list.append(queries.query_string)
+    queries_list=queries_list
+    choice = random.choice(queries_list)
+    assert "Orders(OrderID=264879099)?sap-client=500" == choice
+
+def test_direct_builder_http_post_url():
+    random.seed(20)
+    path_to_metadata = Path(__file__).parent.joinpath("metadata-northwind-v2.xml")
+    metadata_file_contents = path_to_metadata.read_bytes()
+    restrictions = RestrictionsGroup(None)
+    post_builder = DirectBuilder(metadata_file_contents, restrictions,"POST")
+    post_entities = post_builder.build()
+    queryable_factory = SingleQueryable
+    queries_list = []
+    queries_list.clear()
+    for queryable in post_entities.all():
+        entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
+        for _ in range(entityset_urls_count):
+            q = queryable_factory(queryable, logger, 1)
+            queries,body = q.generate()
+            queries_list.append(queries.query_string)
+    queries_list=queries_list
+    choice = random.choice(queries_list)
+    assert "Suppliers?sap-client=500" == choice
+
+def test_direct_builder_body_generation():
+    random.seed(20)
+    path_to_metadata = Path(__file__).parent.joinpath("metadata-northwind-v2.xml")
+    metadata_file_contents = path_to_metadata.read_bytes()
+    restrictions = RestrictionsGroup(None)
+    dir_builder = DirectBuilder(metadata_file_contents, restrictions,"PUT")
+    dir_entities = dir_builder.build()
+    queryable_factory = SingleQueryable
+    body_list = []
+    body_list.clear()
+    for queryable in dir_entities.all():
+        entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
+        for _ in range(entityset_urls_count):
+            q = queryable_factory(queryable, logger, 1)
+            queries,body = q.generate()
+            body_list.append(body)
+    assert random.choice(body_list) == "{\"OrderID\": 264879099, \"CustomerID\": \"PE\", \"EmployeeID\": 1232571814, \"OrderDate\": \"/Date(129712006108)/\", \"RequiredDate\": \"/Date(190417733358)/\", \"ShippedDate\": \"/Date(144052759134)/\", \"ShipVia\": -1121991714, \"Freight\": \"77202251625.6257m\", \"ShipName\": \"\\u00e8r\\u00fefy\\u00f4\\u2020hpD\\u00ff:fVK\\u00e1\\u00f1\\u201d\\u00da^:\", \"ShipAddress\": \"\\u0081\\u00c7\\u00d0F\\u00f0\\u2014\", \"ShipCity\": \"\\u00f6\\u00a1rsH\\u00f6\\u00ac*$\\u00fe\\u00d5\\u00f3\\u00a8\", \"ShipRegion\": \"\\u00eb\", \"ShipPostalCode\": \"d\\u00f2\\u00ae\\u00ff\\u00ea\", \"ShipCountry\": \"ZS\\u00fd\\u0192\\u00c0\"}"
+
+def test_direct_builder_http_merge_body():
+    random.seed(20)
+    path_to_metadata = Path(__file__).parent.joinpath("metadata-northwind-v2.xml")
+    metadata_file_contents = path_to_metadata.read_bytes()
+    restrictions = RestrictionsGroup(None)
+    dir_builder = DirectBuilder(metadata_file_contents, restrictions,"MERGE")
+    dir_entities = dir_builder.build()
+    queryable_factory = SingleQueryable
+    body_list = []
+    body_list.clear()
+    for queryable in dir_entities.all():
+        entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
+        for _ in range(entityset_urls_count):
+            q = queryable_factory(queryable, logger, 1)
+            queries,body = q.generate()
+            body_list.append(body)
+    assert body_list[10] == "{\"PostalCode\": \"L\\u00ccN\\u00d2>ge\"}"
