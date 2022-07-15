81ddb4305b1baa1e944f9fd280ee7f749d18c180
Petr Hanak <petr.hanak@sap.com>
2022-07-15 21:46:58 +0200

    restructuralize: delete config options not used anywhere

--- odfuzz/config.py
+++ odfuzz/config.py
@@ -6,16 +6,12 @@ from odfuzz.constants import (
     DEFAULT_DATA_FORMAT,
     DEFAULT_USE_ENCODER,
     DEFAULT_SAP_CLIENT,
-    DEFAULT_URLS_PER_PROPERTY,
     DEFAULT_IGNORE_METADATA_RESTRICTIONS,
-    DEFAULT_CLI_RUNNER_SEED,
     DEFAULT_SAP_VENDOR_ENABLED,
     ENV_DATA_FORMAT,
     ENV_USE_ENCODER,
     ENV_SAP_CLIENT,
-    ENV_URLS_PER_PROPERTY,
     ENV_IGNORE_METADATA_RESTRICTIONS,
-    ENV_CLI_RUNNER_SEED,
     ENV_SAP_VENDOR_ENABLED,
 )
 
@@ -24,10 +20,7 @@ class FuzzerConfig:
     def __init__(self):
         self._sap_client = os.getenv(ENV_SAP_CLIENT, DEFAULT_SAP_CLIENT)
         self._data_format = os.getenv(ENV_DATA_FORMAT, DEFAULT_DATA_FORMAT)
-        env_url_per_property =  os.getenv(ENV_URLS_PER_PROPERTY, DEFAULT_URLS_PER_PROPERTY)
-        self._urls_per_property = int(env_url_per_property)
         self._ignore_restriction = os.getenv(ENV_IGNORE_METADATA_RESTRICTIONS,DEFAULT_IGNORE_METADATA_RESTRICTIONS)
-        self._cli_runner_seed = os.getenv(ENV_CLI_RUNNER_SEED, DEFAULT_CLI_RUNNER_SEED)
         self._http_method_enabled = "GET"
         self._sap_vendor_enabled = True if os.getenv(ENV_SAP_VENDOR_ENABLED, DEFAULT_SAP_VENDOR_ENABLED) == 'True' else False
 
@@ -49,16 +42,8 @@ class FuzzerConfig:
         return self._data_format
 
     @property
-    def urls_per_property(self):
-        return self._urls_per_property
-
-    @property
     def ignore_restriction(self):
         return self._ignore_restriction
-
-    @property
-    def cli_runner_seed(self):
-        return self._cli_runner_seed
 
     @property
     def http_method_enabled(self):
