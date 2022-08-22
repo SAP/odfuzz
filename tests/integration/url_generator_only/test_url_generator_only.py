ca6375b1d6ab1543ca21f2f88fa94570a33a8f12
Deev Pal <deev.pal@gmail.com>
2022-08-22 15:04:04 +0530

    Exclusion list (#113)
    * Updated Fuzzer.py to implement ExclusionList
    
    * Implemented exclusion_list on DirectBuilder
    
    Implemented a method apply_restrictions in the DirectBuilder class which calls the regex_match function to compare the exclusion_dict which contains all the entities and their respective properties to be excluded while generating  fuzzed URLs by ODFuzz with the set of entities and properties parsed by pyodata from the metadata file.
    
    * Updated entities.py(#113)
    
    1. Converted the line comments to docstrings
    2. Added a setter method to set the restrictions in the DIrectBuilder class
    
    * Update entities.py and test_url_generator.py(#113)
    
    1. Looping directly through _queryable.all() instead of declaring a new variable in the method _apply_restrictions() of the DirectBuilder class in entities.py .
    2. Initially the build() method of DirectBuilder class in enetities.py returned an object of the class QueryableEntities which in turn returned a list of all the parsed entities from the metadata.xml file, but after implementing the feature of Restricting/Excluding the entities and their properties from generating their URLs, the build method now returns a list of entities after restrictions applied to the original list instead of the object of the class QueryableEntities, due to which all the tests where failing locally on the test_url_generator_only.py as all the methods where calling the all() method of the QueryableEntities class to get the list of entities, but their is no method named all() for the list of entities been returned by the build() method of DirectBuilder class and hence removed all the calls to the all() method and directly iterating the list returned by the build() method.

--- tests/integration/url_generator_only/test_url_generator_only.py
+++ tests/integration/url_generator_only/test_url_generator_only.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 import random
+import os
 
 from odfuzz.restrictions import RestrictionsGroup
 from odfuzz.entities import DirectBuilder
@@ -27,15 +28,15 @@ def test_expected_integration_sample():
     entities = builder.build()
 
     ''' uncomment for code sample purposes
-    print('\n entity count: ', len(entities.all()) )
-    for x in entities.all():
+    print('\n entity count: ', len(entities) )
+    for x in entities:
         print(x.__class__.__name__, '  --  ', x.entity_set)
     print('\n--End of listing the parsed entities--')
     '''
 
     queryable_factory = SingleQueryable
 
-    for queryable in entities.all():
+    for queryable in entities:
         URL_COUNT_PER_ENTITYSET = len(queryable.entity_set.entity_type.proprties()) * 1
         #Leaving as 1 instead of default 20, so the test output is more understandable and each property has one URL generated
 
@@ -67,7 +68,7 @@ def test_direct_builder_http_get():
     get_entities , queryable_factory = builder("GET")
     queries_list = []
     queries_list.clear()
-    for queryable in get_entities.all():
+    for queryable in get_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -82,7 +83,7 @@ def test_direct_builder_http_delete():
     del_entities , queryable_factory = builder("DELETE")
     queries_list = []
     queries_list.clear()
-    for queryable in del_entities.all():
+    for queryable in del_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -97,7 +98,7 @@ def test_direct_builder_http_put_url():
     put_entities , queryable_factory = builder("PUT")
     queries_list = []
     queries_list.clear()
-    for queryable in put_entities.all():
+    for queryable in put_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -112,7 +113,7 @@ def test_direct_builder_http_post_url():
     post_entities , queryable_factory = builder("POST")
     queries_list = []
     queries_list.clear()
-    for queryable in post_entities.all():
+    for queryable in post_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -127,7 +128,7 @@ def test_direct_builder_body_generation():
     dir_entities , queryable_factory = builder("PUT")
     body_list = []
     body_list.clear()
-    for queryable in dir_entities.all():
+    for queryable in dir_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -140,7 +141,7 @@ def test_direct_builder_http_merge_body():
     merge_entities , queryable_factory = builder("MERGE")
     body_list = []
     body_list.clear()
-    for queryable in merge_entities.all():
+    for queryable in merge_entities:
         entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
         for _ in range(entityset_urls_count):
             q = queryable_factory(queryable, logger, 1)
@@ -154,3 +155,39 @@ def test_function_imports():
     function_import_list = FunctionImport.get_functionimport_list(path_to_metadata.read_bytes())
     fuzzed_fi = FunctionImport.generate_queries_for_functionimport(function_import_list[0])
     assert fuzzed_fi == "DuplicateCategory?CategoryName='%C2%AA%C2%BA%C3%92d2%C2%B5%C2%BC%E2%80%94%C3%A6Qi%C3%84%C2%BC%C2%81t%5E%C3%BD%E2%80%98l%C2%A7K%C3%99%C2%8F%E2%80%9D%C2%A7%3D%C3%AB_%C2%B2T%C3%AE%3C%C3%A8%E2%80%98J%C2%B2%24%C2%AE%C3%9C%E2%80%9Cl%C3%94b%21JZ%3C%C3%88%E2%80%99%24%E2%80%B0%C3%A9%C3%8B%C2%B1%C2%AC%C2%B7q%C3%A6%C3%94%C2%81%C2%BFP7%C2%A5%24ji%C2%BE%3C%C3%9A%C3%A7s%C3%87uN%E2%80%A2%C3%90%60%C3%98NDz%C2%AFRY%C2%8D%C2%AB%C2%A6%C2%B0%40%C3%AD%E2%80%93L%C3%9D%C3%84UF%2B%C2%B2%3CR%C3%A8%C3%A5TU%C2%B9%C5%92%C2%81%C2%AC7b%C3%8A%C3%B5l%C6%92%C2%A8%40u_%C2%B0%C2%A3%C3%94P%E2%84%A2%C2%BD%C3%AB%24%C3%BB%C3%85-%C3%84%C2%BA%C3%99%C2%BEqR%C2%BC%C3%AB%C2%B5%C2%A2J%C2%A7%C3%89%C3%94%C2%8D%C3%88'&DuplicateName='%C3%B2R%C3%8E%C2%AEA%C2%A6ja%C2%AE%C3%88qm%C2%8DyNC%C3%9A%C2%B2%C2%BBK%C2%B3%C2%AC%C2%BF%C2%B0%E2%80%A1%C3%B8%29%C3%B1%C2%B3%C2%A9%C6%92%C3%A5%C3%84%C3%84O%C3%BC%21%C5%93%C3%9Di%C3%AE0%C3%A3%C3%95%C3%92%C3%9F%C2%B9gE%7C%C2%AC%5E%C3%B6%5Dxl%C3%97%C3%8BC%5BQ%C3%AA%C2%B9L%E2%80%98%C3%97%C2%A2Z%C2%8DVu%C3%96Z%C2%AA3z%C3%AE%3E%C2%B5%C3%BD%C3%BF%C3%B0%C3%84%C3%B7%C2%A9%C2%B0%C3%A9%C3%BD%C3%8D3%2B%C3%97%E2%80%99E%C3%9F%C3%9Fb3%C2%A9'&DuplicationComment='%E2%80%9D%C3%A51r%C2%A9xo%C2%8D%28%E2%80%98%C2%B2%C2%A9%7DZ%C3%9F%C3%BE%3D%C2%B6%C3%B0%7C%C3%80%C2%BE%C3%80_%C2%A3%C2%AF%C3%A2%E2%80%A1%C2%8D%C3%B8%E2%80%93%C2%BA%C2%B0%C2%B9%C3%95%C2%B9v%C3%AF%C3%89%C3%B7%E2%80%99%C3%B7%C2%A3S%C3%99%C2%A6%C3%B7%C3%8Fq%C3%97%C3%BA%3Ee%C3%96%C2%81%C3%A5%C5%93s%C3%BC%C2%A1%C3%9B%60%3D%C3%9AX%24S%3Ez%C3%8Dn6%C3%90%C3%BDs%C2%A7Z%C3%90%C3%91%C2%AE%C3%96e%C2%9D1%E2%80%A0%C3%92Y%C3%84%C3%8F%20%C2%81m%C3%A7%C3%B0%C3%93mj%C3%8F%E2%80%99%C2%A1%C2%81%C3%98%C3%A2SPF%C3%9A%C5%92Rh%C3%B8%C3%88%C3%81%C2%8Dx%C2%A1%C2%AE%C3%92%C2%8DpM%C2%AF%C3%B3%C2%B7%C3%9B%C2%B1%C3%B7%C2%AF%C3%99T%C2%8F%C2%A9%E2%80%A1%C3%8E%C3%82%C2%B3%E2%84%A2Zz%C2%AB%C3%A3OkX%C3%A2%C3%A1Y%C3%90%C2%AA%E2%80%98%C3%8E%E2%80%9Cm%C3%99nR'&DeleteOriginal=true"
+
+#static dictionary for testing the implementation of exclusion_list
+exclusion_dict = {
+                "$ENTITY_SET$":{
+                        "Products": {
+                            "Properties":["ProductID", "ProductName"],
+                            "Nav_Properties":["Category"]
+                        },
+                        "Categories": {
+                            "Properties":[],
+                            "Nav_Properties":[]
+                        }
+                    }
+                }
+
+def test_method_for_exclusion_dict():
+    #Method used for testing the implementation of exclusion_list
+    os.environ["ODFUZZ_IGNORE_METADATA_RESTRICTIONS"] = "True"
+    path_to_metadata = Path(__file__).parent.joinpath("metadata-northwind-v2.xml")
+    metadata_file_contents = path_to_metadata.read_bytes()
+    restrictions = RestrictionsGroup(None, exclusion_dict)
+    del_builder = DirectBuilder(metadata_file_contents, restrictions, "GET")
+    del_entities = del_builder.build()
+    queryable_factory = SingleQueryable    
+    for queryable in del_entities:
+        entityset_urls_count = len(queryable.entity_set.entity_type.proprties())
+        for _ in range(entityset_urls_count):
+            q = queryable_factory(queryable, logger, 1)
+            queries,body = q.generate()
+            print(queries.query_string,"\n")
+            print(body,"\n\n")
+ 
+    #FioriDast update the Exception List
+    restrictions = RestrictionsGroup(None, exclusion_dict)
+    del_builder.set_restrictions(restrictions)
+    del_entities = del_builder.build()
